a84de9b9b789ac00f5118183fcfb0f6dcd4ff338
siegfried1997 <siegfried1997@gmail.com>
2022-11-01 17:34:15 +0400

    balanced bst unit test

---
+++ BalancedBSTUnitTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeFuMonastery;
+using AlgorithmsDataStructures2;
+
+namespace MSTest
+{
+    [TestClass]
+    public class BalancedBSTUnitTest
+    {
+        [TestMethod]
+        public void BalancedBSTStart()
+        {
+            var arr1 = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+            var bstArr = BalancedBST.GenerateBBSTArray(arr1);
+            var arr2 = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+            var bstArr2 = BalancedBST.GenerateBBSTArray(arr2);
+            var test = " ";
+        }
+    }
+}
